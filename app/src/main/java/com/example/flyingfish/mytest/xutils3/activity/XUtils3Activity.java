9e99656208bbb5042c270de43239d446d76d3671
肖婷 <xiao.ting@microcardio.com>
2017-08-09 17:34:09 +0800

    xutils3初始界面搭建

---
+++ app/src/main/java/com/example/flyingfish/mytest/xutils3/activity/XUtils3Activity.java
@@ -0,0 +1,15 @@
+package com.example.flyingfish.mytest.xutils3.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.flyingfish.mytest.R;
+
+public class XUtils3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xutils3);
+    }
+}
